8769e8e2bb6a2cdfd108453448e3050cc307b046
Matheus-9 <matheusexu@gmail.com>
2023-11-02 09:48:30 -0300

    Criando nova Branch (CadastroVoluntari) e adicioando metodos get e set a classe CadastroVoluntario

--- src/ampare-backend/Models/CadastroVoluntario.cs
+++ src/ampare-backend/Models/CadastroVoluntario.cs
@@ -5,5 +5,9 @@ namespace ampare_backend.Models
     [Table("Cadastro_voluntario")]
     public class CadastroVoluntario
     {
+        [Key]
+        public int Id { get; set; }
+        public string Nome {get; set; }
+        public int Idade {get; set; }
     }
 }
