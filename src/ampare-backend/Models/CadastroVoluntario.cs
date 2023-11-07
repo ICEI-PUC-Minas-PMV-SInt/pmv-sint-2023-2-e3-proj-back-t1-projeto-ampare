51379fc18041dc2b256f7477b6cd43bdf45aa3c4
Matheus Soares de Sales <85132779+Matheus-9@users.noreply.github.com>
2023-11-07 19:45:51 -0300

    Criando nova Branch (CadastroVoluntario) e adicionando metodos get e set a classe CadastroVoluntario (#90)

--- src/ampare-backend/Models/CadastroVoluntario.cs
+++ src/ampare-backend/Models/CadastroVoluntario.cs
@@ -4,5 +4,9 @@ namespace ampare_backend.Models
 {
     public class CadastroVoluntario
     {
+        [Key]
+        public int Id { get; set; }
+        public string Nome {get; set; }
+        public int Idade {get; set; }
     }
 }
