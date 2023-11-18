a8fb19fd09173bdf1a5de3ed2178a515934af9ba
Matheus <Matheus@DESKTOP-QOIFRA4>
2023-11-18 18:52:31 -0300

    adiciando o cadstro volunatio aos migration, e atualizando o banco de dados.

---
+++ src/ampare-backend/Migrations/20231118214623_add-TableCadastroVoluntario.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ampare_backend.Migrations
+{
+    public partial class addTableCadastroVoluntario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
