a6771299c19ff47fea311522ed1a64318ec5125b
Raquel Bomjardim <bomjardimraquel@gmail.com>
2023-11-18 12:12:10 -0300

    Atualização da classe CadastroOng

--- src/ampare-backend/Models/CadastroOng.cs
+++ src/ampare-backend/Models/CadastroOng.cs
@@ -1,8 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ampare_backend.Models
 {
     public class CadastroOng
     {
+        [Key]
+        public int Id {get; set; }
+        public string Nome {get; set; }
+        public string Email {get; set; }
+        public string Endereço {get; set; }
+        public string Telefone {get; set; }
     }
 }
