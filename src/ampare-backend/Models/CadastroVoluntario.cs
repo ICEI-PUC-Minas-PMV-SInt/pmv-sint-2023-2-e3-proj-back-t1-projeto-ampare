3d21b98a6fc4e0ad8e91aeaf08fe8498bfeeaf20
Matheus Soares de Sales <85132779+Matheus-9@users.noreply.github.com>
2023-11-19 11:57:07 -0300

    adiciondo o cadastro voluntario aos migration, e atualizando o banco de… (#96)
    * adiciando o cadstro volunatio aos migration, e atualizando o banco de dados.
    
    * Criando o contrlller da class (Cadastro Voluntario)
    
    ---------
    
    Co-authored-by: Matheus <Matheus@DESKTOP-QOIFRA4>

--- src/ampare-backend/Models/CadastroVoluntario.cs
+++ src/ampare-backend/Models/CadastroVoluntario.cs
@@ -1,7 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ampare_backend.Models
 {
+
+    [Table("Cadatro_Voluntarios")]
     public class CadastroVoluntario
     {
         [Key]
